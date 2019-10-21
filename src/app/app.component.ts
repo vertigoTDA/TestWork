1971a42154459c2afb1e6f37483436507083923c
VertigoTDA <git@github.com:vertigoTDA>
2019-10-21 22:13:21 +0300

    create JSON get request, creating table by JSON information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Test21102019';
+
+export class AppComponent implements OnInit{
+  private apiTableData: any;
+
+  constructor(
+    private httpClient: HttpClient
+    ) {
+
+  }
+
+  ngOnInit() {
+    this.getDataFromServer();
+  }
+
+  getDataFromServer() {
+    const tableApiUrl = 'https://smida-dev.test.idoc.com.ua/api/registry/okpoCard';
+    this.httpClient.get(
+      tableApiUrl,
+      {
+        params: { code: '01130549' }
+      }
+      ).subscribe(
+      (result) => {
+        this.apiTableData = result;
+        console.log(result);
+      }
+      );
+  }
+
+
 }
